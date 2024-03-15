cad0fce7e7f9f5c54a4d66fd23414b3d5d803d8b
Nivaash <nidishnivash@gmail.com>
2024-03-15 15:14:38 +0530

    Landing Page for the Capstone has been Created

---
+++ Client/communityconnect/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import LandingPage from './Components/LandingPage'
+import NavBar from './Components/NavBar'
+
+function App() {
+
+  return (
+    <>
+      {/* <NavBar/> */}
+      <LandingPage/>
+    </>
+  )
+}
+
+export default App
