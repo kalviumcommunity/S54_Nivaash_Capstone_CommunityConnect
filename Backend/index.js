795f1e753f23e3f0f4ee83dcd0ba4e3525bd4a2a
Nivaash <nidishnivash@gmail.com>
2024-04-01 12:54:24 +0530

    Deployment Fix

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 const PORT = 3000;
 const cors = require("cors"); 
@@ -9,6 +11,15 @@ const volunteerRouter = require('./Routes/volunteerRouter');
 app.use(errorHandler);
 app.use(cors());
 app.use(express.json());
+
+async function connectDatabase() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log('Connected to Database!!!');
+    } catch (error) {
+        console.error('Error connecting to Database:', error);
+    }
+}
 
 app.get("/", (req, res) => {
     connectDatabase()
