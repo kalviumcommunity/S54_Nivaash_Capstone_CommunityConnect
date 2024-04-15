b52a506c2e35145219e4549b47ac6a920ec8493b
Nivaash <nidishnivash@gmail.com>
2024-04-15 15:54:27 +0530

    Client Side Routing for Navbar and Volunteer Page

--- Client/communityconnect/src/App.jsx
+++ Client/communityconnect/src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
   return (
     <>
-      {/* <AllRoutes/> */}
-      <VolunteerProfile/>
+      <AllRoutes/>
+      {/* <VolunteerProfile/> */}
     </>
   )
 }
--- Client/communityconnect/src/Components/NavBar.jsx
+++ Client/communityconnect/src/Components/NavBar.jsx
@@ -18,7 +18,9 @@ const NavBar = () => {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="white"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-[#2b2c43] rounded-box w-52">
-            <li><a className="text-white">Home</a></li>
+            <Link to="/" >
+              <li><p className="text-white">Home</p></li> 
+            </Link>
             <li>
               <a className="text-white">About</a>
               <ul className="p-2">
@@ -35,7 +37,9 @@ const NavBar = () => {
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a className="text-xl text-white">Home</a></li>
+          <Link to="/" >
+            <li><p className="text-xl text-white">Home</p></li>
+          </Link>
           <li>
             <details>
               <summary className="text-xl text-white">About</summary>
