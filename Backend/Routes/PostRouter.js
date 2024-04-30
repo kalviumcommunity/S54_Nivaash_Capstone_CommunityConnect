4624b42d72cae00a5543883bf09105b7b1baadbf
Nivaash <nidishnivash@gmail.com>
2024-04-30 15:10:30 +0530

    deployment fix

--- Backend/Routes/PostRouter.js
+++ Backend/Routes/PostRouter.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const postController = require('../Controllers/postController.js');
+const postController = require('../Controllers/PostController.js');
 
 router.post('/create', postController.createPost);
 
